a1337340e0e310ddf7913f714e73ec5f51be3a89
evanpaddock <edpaddock@crimson.ua.edu>
2023-09-22 13:15:35 -0500

    Init

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FL2023_MIS521_Assignment2_epaddock;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
+
+        app.Run();
+    }
+}
+
