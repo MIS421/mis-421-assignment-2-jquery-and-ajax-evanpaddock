1cfeed601a0e83a8ec28da9e8abac556360d9c31
evanpaddock <edpaddock@crimson.ua.edu>
2023-09-26 13:22:34 -0500

    API & Key

--- wwwroot/script.js
+++ wwwroot/script.js
@@ -10,9 +10,9 @@ function apiSearch() {
   };
 
   $.ajax({
-      url: 'my-api-url' + $.param(params),
+      url: 'https://api.bing.microsoft.com/v7.0/search?' + $.param(params),
       beforeSend: function (xhrObj) {
-        xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "my-api-key");
+          xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "7d3db6fafd0f4ac48084bb0edafc8df5");
       },
       type: "GET",
     })
